df2110ffad1258f2a7f8548f9cb62816a6684184
Cristhian Pinzon <cristhianpinzon@MacBook-Air-de-Cristhian.local>
2017-04-03 13:40:32 -0500

    Primeros Cambios

---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<title>Curso Materialize CSS</title>
+	<link rel="stylesheet" type="text/css" href="css/materialize.min.css">
+</head>
+<body>
+
+	<nav>
+    <div class="nav-wrapper">
+      <a href="#" class="brand-logo right">Materialize CSS</a>
+      <ul id="nav-mobile" class="left hide-on-med-and-down">
+        <li><a href="">Inicio</a></li>
+        <li><a href="">Acerca</a></li>
+        <li><a href="">Contacto</a></li>
+      </ul>
+    </div>
+  </nav>
+
+  <!-- SIEMPRE HAY QUE TENER UN CONTENEDOR PRINCPIAL  - GRIDS -->
+
+  <div class="container">
+
+  	
+    <!-- Navbar goes here -->
+
+    <!-- Page Layout here -->
+    <div class="row">
+
+      <div class="col s12 m4 l3"> <!-- Note that "m4 l3" was added -->
+        <!-- Grey navigation panel
+
+              This content will be:
+          3-columns-wide on large screens,
+          4-columns-wide on medium screens,
+          12-columns-wide on small screens  -->
+
+      </div>
+
+      <div class="col s12 m8 l9"> <!-- Note that "m8 l9" was added -->
+        <!-- Teal page content
+
+              This content will be:
+          9-columns-wide on large screens,
+          8-columns-wide on medium screens,
+          12-columns-wide on small screens  -->
+
+      </div>
+
+    </div>
+          
+
+
+  </div>
+        
+	<script type="js/materialize.js"></script>
+	<script type="js/jquery.js"></script>
+</body>
+</html>
